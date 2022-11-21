52dc97982fb90506660156e3adfee45376777c30
igabriel-gb <gabrielgb.work@gmail.com>
2022-11-21 19:45:59 -0300

    primeiras aulas do curso de python ate o momento sem duvidas, aula bem tranquila

---
+++ modulo_1.py/aula_2.py
@@ -0,0 +1,33 @@
+valor_passagem = 4.30
+valor_corida = input('MODELO 1    qual o valor da corrida?  ')
+
+if float(valor_corida) <= valor_passagem * 5:
+    print("pague a corrida")
+if float(valor_corida) > valor_passagem * 5:
+    print("pegue o ônibus")
+
+
+
+
+valor_passagem = 4.30
+valor_corida = input('modelo 2    QUAL O VALOR DA CORRIDA?  ')
+
+if float(valor_corida) <= valor_passagem * 5:
+    print("pague a corrida")
+else:
+    if float(valor_corida) <= valor_passagem * 6:
+        print("espere um pouco o valor da corrida pode abaixar")
+    else:
+        print("pague o ônibus")
+
+
+
+        valor_passagem = 4.30
+valor_corida = input('Modelo 3    Qual O Valor Da Corrida?  ')
+
+if float(valor_corida) <= valor_passagem * 5:
+    print("pague a corrida")
+elif float(valor_corida) <= valor_passagem * 6:
+    print("espere um pouco o valor da corrida pode abaixar")
+else:
+    print("pague o ônibus")
